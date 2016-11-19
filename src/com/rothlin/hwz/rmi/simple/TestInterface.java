ae5917a88179959249f91f5d8fda6c544cc51efd
Sandro Ciervo <sandro.ciervo@live.com>
2016-11-19 20:16:21 +0000

    Lösungsfremde Packages entfernen

--- src/com/rothlin/hwz/rmi/simple/TestInterface.java
+++
@@ -1,8 +0,0 @@
-package com.rothlin.hwz.rmi.simple;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface TestInterface extends Remote {
-	public boolean isLoginValid(String username, String password) throws RemoteException;
-}
--- src/com/rothlin/hwz/rmi/simple/Test_RMI_Definitions.java
+++
@@ -1,7 +0,0 @@
-package com.rothlin.hwz.rmi.simple;
-
-public class Test_RMI_Definitions {
-	public static final String RMI_ServerHost = "localhost";
-	public static final String RMI_ID         = "TestRMISimple";
-	public static final int    RMI_PORT       = 222;
-}
